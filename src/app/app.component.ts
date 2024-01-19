2ab9a75d59a75de1449c42d4e20fdb9046246b90
Nelson Guerra <nelsonguerra.tech@outlook.com>
2024-01-19 11:45:18 -0500

    add animate on scroll to project, setup main page and master styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SkillsComponent } from "./components/skills/skills.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavigationComponent,
+    LandingPageComponent,
+    ProjectsComponent,
+    FooterComponent,
+    SkillsComponent,
+  ],
 })
 export class AppComponent {
-  title = 'ios';
+  title = "ios";
 }
