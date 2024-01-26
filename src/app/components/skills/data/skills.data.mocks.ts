fe6edecd3a2c75d124d37029267ed8236bb254a7
Nelson Guerra <nelsonguerra.tech@outlook.com>
2024-01-26 15:48:17 -0500

    create skills component and add html, css, interfaces, and mocks.

---
+++ src/app/components/skills/data/skills.data.mocks.ts
@@ -0,0 +1,26 @@
+import { Skills } from "../models/skills.interface";
+
+export const SkillsDataMocks: Skills = {
+  skills: [
+    {
+      src: "../../../../assets/skills/figma/figma.png",
+      alt: "Figma",
+    },
+    {
+      src: "../../../../assets/skills/dribbble/dribbble.png",
+      alt: "Dribbble",
+    },
+    {
+      src: "../../../../assets/skills/miro/miro.png",
+      alt: "Miro",
+    },
+    {
+      src: "../../../../assets/skills/gitlab/gitlab.png",
+      alt: "GitLab",
+    },
+    {
+      src: "../../../../assets/skills/story/storybook.png",
+      alt: "Storybook",
+    },
+  ],
+};
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Skills } from "./models/skills.interface";
+import { SkillsDataMocks } from "./data/skills.data.mocks";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-skills',
+  selector: "app-skills",
   standalone: true,
-  imports: [],
-  templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  imports: [CommonModule],
+  templateUrl: "./skills.component.html",
+  styleUrl: "./skills.component.scss",
 })
 export class SkillsComponent {
-
+  @Input() content: Skills = SkillsDataMocks;
 }
